558ddf9bf8a58c7786c87358e40434dfd44b6c7a
jagodasad <jago.sadowska@gmail.com>
2021-11-25 09:36:40 +0100

    added dictionary Api functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 import logo from "./logo.mp4";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <video className="logo" autoPlay loop muted id="logo">
-          <source src={logo} type="video/mp4" />
-        </video>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <video className="logo" autoPlay loop muted id="logo">
+            <source src={logo} type="video/mp4" />
+          </video>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <a
+            href="https://github.com/jagodasad/weather-react"
+            target="_blank"
+            className="created-by"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          This project was coded by{" "}
+          <strong>
+            <a
+              href="https://www.linkedin.com/in/jagoda-sadowska-5352bb9b"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Jagoda Sadowska
+            </a>
+          </strong>
+        </footer>
+      </div>
     </div>
   );
 }
